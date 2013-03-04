3c29866ebb712a7f6e46232e3dfe31232dcb2d0f
xiaoma <xiaoma@unknown7cd1c3dd63c9.att.net>
2013-03-04 01:53:56 -0800

    adds repeats to musexpr syntax and more tests

--- compileNotes.js
+++ compileNotes.js
@@ -6,7 +6,8 @@ var endTime = function (time, expr) {
 
 // reverses an abstract syntax tree of musical expressions
 var reverse = function(expr) {
-  if (expr.tag === 'note') return expr;
+  if ((expr.tag === 'note') || (expr.tag === 'par') ||
+    (expr.tag === 'repeat')) return expr;
   else return {
     tag: 'seq',
     left: reverse(expr.right),
@@ -32,13 +33,18 @@ var compile = function (musexpr, start) {
     compile(musexpr.right, start).forEach(function(x) {expr.push(x);});
   } else if (musexpr.tag === 'par') {
     compile(musexpr.left, start).forEach(function(x) {expr.push(x);});
-    compile(musexpr.right, start).forEach(function(x) {expr.push(x);});    }
+    compile(musexpr.right, start).forEach(function(x) {expr.push(x);});
+  } else if (musexpr.tag === 'repeat') {
+    _(musexpr.count).times(function() {
+      expr.push(musexpr.section);
+    });
+  }
   return expr;
 };
 
 var noteToMidi = function(note) {
   if (!/[A-G][0-8]/.test(note)) throw("Not a valid note!  Must be a note between A0 and C8");
   var notes = {C: 0, D: 2, E:4, F: 5, G:7, A:9, B:11};
-  var octave = parseInt(note[1]) + 1;
+  var octave = parseInt(note[1], 10) + 1;
   return octave * 12 + notes[note[0]];
 };
--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -5,9 +5,9 @@ describe("Compile Notes", function() {
   beforeEach(function() {
     melody1_mus = { tag: 'note', pitch: 'a4', dur: 125 };
     melody1_note = [ { tag: 'note', pitch: 'a4', start: 0, dur: 125 } ];
-    melody2_mus = 
+    melody2_mus =
     { tag: 'seq',
-      left: 
+      left:
        { tag: 'seq',
          left: { tag: 'note', pitch: 'a4', dur: 250 },
          right: { tag: 'note', pitch: 'b4', dur: 250 } },
@@ -16,9 +16,9 @@ describe("Compile Notes", function() {
          left: { tag: 'note', pitch: 'c4', dur: 500 },
          right: { tag: 'note', pitch: 'd4', dur: 500 } } };
 
-    melody3_mus = 
+    melody3_mus =
     { tag: 'seq',
-      left: 
+      left:
        { tag: 'seq',
          left: { tag: 'note', pitch: 'd4', dur: 500 },
          right: { tag: 'note', pitch: 'c4', dur: 500 } },
@@ -33,9 +33,9 @@ describe("Compile Notes", function() {
       { tag: 'note', pitch: 'c4', start: 500, dur: 500 },
       { tag: 'note', pitch: 'd4', start: 1000, dur: 500 } ];
 
-    parallel_mus = 
+    parallel_mus =
     { tag: 'seq',
-      left: 
+      left:
        { tag: 'par',
          left: { tag: 'note', pitch: 'c3', dur: 250 },
          right: { tag: 'note', pitch: 'g4', dur: 500 } },
@@ -43,12 +43,15 @@ describe("Compile Notes", function() {
        { tag: 'par',
          left: { tag: 'note', pitch: 'd3', dur: 500 },
          right: { tag: 'note', pitch: 'f4', dur: 250 } } };
-    
+
     parallel_notes = [
       { tag: 'note', pitch: 'c3', start: 0, dur: 250 },
       { tag: 'note', pitch: 'g4', start: 0, dur: 500 },
       { tag: 'note', pitch: 'd3', start: 500, dur: 500 },
       { tag: 'note', pitch: 'f4', start: 500, dur: 250 } ];
+
+    rest = { tag: 'rest', dur: 250};
+    repeated_note = { tag: 'repeat', section: melody1_note[0], count: 3 };
   });
 
   describe("reverse sytax tree", function() {
@@ -60,18 +63,28 @@ describe("Compile Notes", function() {
       expect(reverse(melody2_mus)).toEqual(melody3_mus);
     });
   });
-  
 
-  it("A sequence with a single note should return and array with the note", function() {
+  describe("Musical expression compiler", function() {
+    it("A sequence with a single note should return and array with the note", function() {
 
-    expect(compile(melody1_mus)).toEqual(melody1_note);
-  });
+      expect(compile(melody1_mus)).toEqual(melody1_note);
+    });
 
-  it("A complex musical expression should compile to notes", function() {
-    expect(compile(melody2_mus)).toEqual(melody2_notes);
-  });
+    it("A complex musical expression should compile to notes", function() {
+      expect(compile(melody2_mus)).toEqual(melody2_notes);
+    });
 
-  it("A sequence with parallel notes and sequences whould compile correctly", function() {
-    expect(compile(parallel_mus)).toEqual(parallel_notes);
+    it("A sequence with parallel notes and sequences should compile correctly", function() {
+      expect(compile(parallel_mus)).toEqual(parallel_notes);
+    });
+
+    it("A sequence with a rest should compile to a rest", function() {
+      expect(compile(rest)).toEqual([rest]);
+    });
+
+    it("A sequence with a 'repeat' should compile to multiple copies of that note", function() {
+      var note = repeated_note.section; // repeated note defined with count=3
+      expect(compile(repeated_note)).toEqual([note, note, note]);
+    });
   });
 });
